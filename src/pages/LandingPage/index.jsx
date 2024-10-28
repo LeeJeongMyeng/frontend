fbd4f9537d28165145006911f08b75bdf1a88c9a
Lee Jeongmyeong <aoddl56@nate.com>
2024-10-28 15:02:47 +0900

    Changes

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const LandingPage = () => {
-  return (
-      <div>
-
-      </div>
-  );
+	return (
+		<div>
+			LandingPage
+		</div>
+	);
 };
 
 export default LandingPage;
